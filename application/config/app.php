dd03530977b6fbe7b4138b6730eda7b9e94961bd
Jonathan Reyes <contact@jonathanreyes.com>
2012-01-26 11:48:22 -0800

    Added boilerplate css normalizing and modernizer for html5 compatibility

--- application/config/app.php
+++ application/config/app.php
@@ -5,6 +5,7 @@ return array
 	// Javascript libraries and initial loaders
 	'scripts' => array(
 		'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
+		'media/js/libs/modernizr-2.0.6.js',
 	),
 	
 	// CSS frameworks and initial styles
--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -1,19 +1,28 @@
 <!doctype html>
+	
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="<?php echo I18n::lang(); ?>"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="<?php echo I18n::lang(); ?>"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="<?php echo I18n::lang(); ?>"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="<?php echo I18n::lang(); ?>"> <!--<![endif]-->
+
 <head>
-		
+	
 	<title></title>
-		
+	
 	<meta name="keywords" content="">
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta charset="utf-8">
 	
 	<link rel="author" href="humans.txt" />
 	
 	<?php foreach ($scripts as $script): ?>
 		<?php echo HTML::script($script); ?>
 	<?php endforeach; ?>
-		
+	
 	<?php foreach ($stylesheets as $style): ?>
 		<?php echo HTML::style($style); ?>
 	<?php endforeach; ?>
