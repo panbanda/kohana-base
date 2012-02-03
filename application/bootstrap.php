a3ebc591a084d6332723380d3adca2dd28ea5b1f
Jonathan Reyes <contact@jonathanreyes.com>
2012-02-02 16:54:11 -0800

    Added minify

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -103,7 +103,8 @@ Kohana::modules(array(
 	'database'   => MODPATH.'database',   // Database access
 	'image'      => MODPATH.'image',      // Image manipulation
 	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
-	'pagination' => MODPATH.'pagination',        // Object Relationship Mapping
+	'pagination' => MODPATH.'pagination',
+	'minify'     => MODPATH.'minify',
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
 	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
 	));
