5aecd0076619168799aafe00a790e7c4de6f327d
Jonathan Reyes <contact@jonathanreyes.com>
2012-02-26 23:08:35 -0800

    Adding 404 error handler and error controller

---
+++ application/classes/controller/errors.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Errors extends Controller_Base_Site {
+
+	public function action_404()
+	{
+		
+	}
+
+}
---
+++ application/views/errors/404.php
@@ -0,0 +1,1 @@
+404 - Page cannot be found.
