e5f5029533723528e3b60dd2c26d310ac43425f8
Jonathan Reyes <contact@jonathanreyes.com>
2012-03-20 15:30:13 -0700

    Updated input to allow for extraction of post / get variables

--- application/classes/input.php
+++ application/classes/input.php
@@ -5,31 +5,39 @@ class Input {
 	/**
 	 * Returns a value from the $_POST array
 	 */
-	public static function post($key = NULL, $default = NULL) 
+	public static function post($key = NULL, $default = NULL)
 	{
-		// if there are no post variables
+		// If there are no post variables
 		if (empty($_POST) && $key === NULL) return FALSE;
-	
-		// if the key is not specified
+
+		// If the key is not specified
 		if (is_null($key)) return $_POST;
-		
-		// perform default
+
+		// Lets load provided variables
+		if (is_array($key)) return Arr::extract($_POST, $key);
+
+		// Perform default
 		return Arr::get($_POST, $key, $default);
 	}
-	
+
 	/**
 	 * Returns a value from the $_GET array
+	 *
+	 * @param  $key  Mixed (NULL, String, Array)
+	 * @param  $default  Default value for individual key
 	 */
-	public static function get($key = NULL, $default = NULL) 
+	public static function get($key = NULL, $default = NULL)
 	{
-		// if there are no post variables
+		// If there are no post variables
 		if (empty($_GET) && $key === NULL) return FALSE;
-	
-		// if the key is not specified
+
+		// If the key is not specified
 		if (is_null($key)) return $_GET;
-	
-		// perform default
+
+		// Lets load provided variables
+		if (is_array($key)) return Arr::extract($_GET, $key);
+
+		// Perform default
 		return Arr::get($_GET, $key, $default);
 	}
-} 
-
+}
